d4c56c0ce73bfb0a04a2d33d6199bea5d0d2d27b
guardianprime <okongordian@gmail.com>
2026-07-25 04:05:07 +0100

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://guardianprime.github.io',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [sitemap()]
+  integrations: [sitemap()],
 });
